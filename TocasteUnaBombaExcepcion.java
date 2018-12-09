e78cc394b6ae22a8aa4188cfb41ea7b2b9de26d2
BrayanCaro <41640423+BrayanCaro@users.noreply.github.com>
2018-12-09 02:29:37 -0600

    Versión definitiva
    Después de todos mis cambios los siguientes serán mínimos, los prometo.

---
+++ TocasteUnaBombaExcepcion.java
@@ -0,0 +1,15 @@
+/**
+ * Excepcion para el manejo de bombas en una celda
+ * @author Brayan Martinez Santana
+ * @version @version Primera version, Lunes 3 de Diciembre, 2018
+ */
+
+public class TocasteUnaBombaExcepcion extends Exception {
+private static final long serialVersionUID = 42l;
+
+public TocasteUnaBombaExcepcion(String a){
+	super(a);
+}
+
+}
+
