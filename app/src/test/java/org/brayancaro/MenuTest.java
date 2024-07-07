9421bb0522aaf3f545514734d7f647fb03001eb0
Brayan Martínez Santana <brayanmtz@ciencias.unam.mx>
2024-07-06 20:58:34 -0600

    test: Fix failing test non related with the proyect

--- app/src/test/java/org/brayancaro/MenuTest.java
+++ app/src/test/java/org/brayancaro/MenuTest.java
@@ -5,39 +5,20 @@ package org.brayancaro;
 
 import static org.junit.Assert.*;
 
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 import org.junit.Test;
 
 public class MenuTest {
 
-    // @Test
-    // public void menuFailsWithInvalidInput() {
-    //     var classUnderTest = new Menu()
-    //         .setScanner(new Scanner("foo"));
-
-    //     assertThrows(
-    //         Exception.class,
-    //             () -> classUnderTest.play()
-    //     );
-    // }
-
     @Test
-    public void valid_scanner_data() {
-        try (var q = new Scanner("(1,2)\n")) {
-            String next = q.next("\\(\\d+\\s*,\\s*\\d+\\)");
+    public void menuFailsWithInvalidInput() {
+        var classUnderTest = new Menu()
+            .setScanner(new Scanner("foo"));
 
-            assertEquals("(1,2)", next);
-        }
-    }
-
-    @Test
-    public void invalid_scanner_data() {
-        try (var q = new Scanner("(1 , 1)")) {
-                q.next("\\(\\d+\\s*,\\s*\\d+\\)");
-            // assertThrows(NoSuchElementException.class, () -> {
-            // });
-        }
+        assertThrows(
+            Exception.class,
+                () -> classUnderTest.play()
+        );
     }
 }
